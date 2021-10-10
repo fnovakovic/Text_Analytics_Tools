46d1e4157786f739cdd97f943f1c88bc25c0bbc2
antic <anta.stefan@gmail.com>
2021-10-10 14:36:12 +0200

    Add post components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {PostListComponent} from "./post-list/post-list.component";
+import {SinglePostComponent} from "./single-post/single-post.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "posts",
+    component: PostListComponent
+  },
+  {
+    path: "posts/:id",
+    component: SinglePostComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PostListComponent } from './post-list/post-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import { SinglePostComponent } from './single-post/single-post.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostListComponent,
+    SinglePostComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
